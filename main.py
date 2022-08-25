dc53b66caab05dfcd30274cd17b17e76e1a8cd5b
Leroy Sheward <leroy.sheward@elev.ga.ntig.se>
2022-08-25 13:26:59 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from unicodedata import name
 
-
-name = input (Hej, )
+Program = "information writer"
+name = input("hello, name?")
+print("welcome", name, ".")
+age = input("age?")
+print(f"age {age} confirmed")
